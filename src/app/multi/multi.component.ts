6061bbbcd7d1325256b92e1d9914afc5f0765331
Yumadilov Timur <t.yumadilov@mail.ru>
2020-11-17 17:46:21 +0500

    multi component

--- src/app/multi/multi.component.ts
+++ src/app/multi/multi.component.ts
@@ -1,41 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {SelectItem} from 'primeng/api';
-
-@Component({
-  selector: 'app-multi',
-  template: `
-    <p-multiSelect ></p-multiSelect>
-  `,
-  styleUrls: ['./multi.component.scss']
-})
-export class MultiComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
 
 interface City {
   name: string,
   code: string
 }
 
-export class MyModel {
+@Component({
+  selector: 'app-multi',
+  templateUrl: './multi.template.html',
+  styleUrls: ['./multi.component.scss']
+})
+export class MultiComponent {
 
   cities1: SelectItem[];
 
+  cities2: City[];
+
   selectedCities1: City[];
 
+  selectedCities2: City[];
+
   constructor() {
-      //SelectItem API with label-value pairs
       this.cities1 = [
           {label:'New York', value:{id:1, name: 'New York', code: 'NY'}},
           {label:'Rome', value:{id:2, name: 'Rome', code: 'RM'}},
           {label:'London', value:{id:3, name: 'London', code: 'LDN'}},
           {label:'Istanbul', value:{id:4, name: 'Istanbul', code: 'IST'}},
           {label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}}
+      ];
+
+      this.cities2 = [
+          {name: 'New York', code: 'NY'},
+          {name: 'Rome', code: 'RM'},
+          {name: 'London', code: 'LDN'},
+          {name: 'Istanbul', code: 'IST'},
+          {name: 'Paris', code: 'PRS'}
       ];
   }
 
